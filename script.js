dfe4f6def4dfefcb32b8631591c9f4cf5f493cee
sohanmatsa <sohanmatsa1@gmail.com>
2024-06-19 11:46:56 +0530

    Added "Score system" to the game and made some UI changes

--- script.js
+++ script.js
@@ -5,44 +5,65 @@ const endMessage = document.getElementById("end-msg")
 const rockMenuImage = document.getElementById("rock-img")
 const paperMenuImage = document.getElementById("paper-img")
 const scissorsMenuImage = document.getElementById("scissors-img")
-const parentElement = document.getElementById("menu")
+const menu = document.getElementById("menu")
 const choice = [rockMenuImage, paperMenuImage, scissorsMenuImage]
+let playerPoints = 4
+let computerPoints = 4
+const playerScore = document.getElementById("player-score")
+const computerScore = document.getElementById("computer-score")
 
-parentElement.addEventListener("click", function (image) {
-    playerImage.src = "images/rock.png";
-    computerImage.src = "images/rock.png";
-    playerImage?.classList.add("leftshake-animation")
-    computerImage?.classList.add("rightshake-animation")
+rockMenuImage.addEventListener("click",gameChoice)
+paperMenuImage.addEventListener("click",gameChoice)
+scissorsMenuImage.addEventListener("click",gameChoice)
+
+function gameChoice(image) {
+    playerImage.src = "images/rock.png"
+    computerImage.src = "images/rock.png"
+    playerImage.classList.add("leftshake-animation")
+    computerImage.classList.add("rightshake-animation")
     message.textContent = "Wait..."
 
     setTimeout(function () {
-        playerImage?.classList.remove("leftshake-animation")
-        computerImage?.classList.remove("rightshake-animation")
+        playerImage.classList.remove("leftshake-animation")
+        computerImage.classList.remove("rightshake-animation")
 
-        const playerChoice = image.target;
-        playerImage.src = playerChoice.src;
+        const playerChoice = image.target
+        playerImage.src = playerChoice.src
 
         const computerChoice = choice[Math.floor(Math.random() * 3)];
-        computerImage.src = computerChoice.src;
+        computerImage.src = computerChoice.src
 
         playGame()
         
     }, 2000)
-})
+}
 
 
 function playGame() {
-
+    
     if (playerImage.src === computerImage.src) {
         message.textContent = "Draw"
     } else if (playerImage.src.includes("rock") && computerImage.src.includes("paper") ||
         playerImage.src.includes("paper") && computerImage.src.includes("scissors") ||
         playerImage.src.includes("scissors") && computerImage.src.includes("rock")) {
         message.textContent = "Computer Win 🤖"
+        computerPoints++
     } else {
         message.textContent = "Player Win 😄"
+        playerPoints++
     }
 
+    playerScore.textContent = "Player : " + playerPoints
+    computerScore.textContent = "Computer : " + computerPoints
+    
+    endGame()
+}
+
+function endGame() {
+    if (playerPoints === 5 || computerPoints === 5){
+        endMessage.hidden = false
+        menu.style.display = "none"
+    }
 }
 
 
